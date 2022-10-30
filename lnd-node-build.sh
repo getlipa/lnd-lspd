7954a356284bfd0ad7e2a1cf491da0afdb5ccbcf
Yaacov Akiba Slama <ya@slamail.org>
2022-10-30 21:36:30 +0200

    Add signrpc and invoicesrpc tags to support Lightning Terminal

--- lnd-node-build.sh
+++ lnd-node-build.sh
@@ -9,5 +9,5 @@ PKG=github.com/lightningnetwork/lnd
 export CGO_ENABLED=0
 export GOOS=linux
 export GOARCH=amd64
-go build -trimpath -ldflags "-X ${PKG}/build.Commit=${COMMIT} -X ${PKG}/build.CommitHash=${COMMIT_HASH} -s -w" -tags=submarineswaprpc,chanreservedynamic,routerrpc,walletrpc,chainrpc -o lnd ./cmd/lnd
-go build -trimpath -ldflags "-X ${PKG}/build.Commit=${COMMIT} -X ${PKG}/build.CommitHash=${COMMIT_HASH} -s -w" -tags=submarineswaprpc,chanreservedynamic,routerrpc,walletrpc,chainrpc -o lncli ./cmd/lncli
+go build -trimpath -ldflags "-X ${PKG}/build.Commit=${COMMIT} -X ${PKG}/build.CommitHash=${COMMIT_HASH} -s -w" -tags=submarineswaprpc,chanreservedynamic,routerrpc,walletrpc,chainrpc,signrpc,invoicesrpc -o lnd ./cmd/lnd
+go build -trimpath -ldflags "-X ${PKG}/build.Commit=${COMMIT} -X ${PKG}/build.CommitHash=${COMMIT_HASH} -s -w" -tags=submarineswaprpc,chanreservedynamic,routerrpc,walletrpc,chainrpc,signrpc,invoicesrpc -o lncli ./cmd/lncli
